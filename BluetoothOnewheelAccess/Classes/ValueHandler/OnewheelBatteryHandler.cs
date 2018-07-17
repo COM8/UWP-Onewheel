f066cae49c7268381fe4e8b26ebc2c4aa128ec76
COM8 <sauter.fabian@mailbox.org>
2018-07-17 13:38:28 +0200

    Fixed reporting at every battery change and added a 100% charge notification

--- BluetoothOnewheelAccess/Classes/ValueHandler/OnewheelBatteryHandler.cs
+++ BluetoothOnewheelAccess/Classes/ValueHandler/OnewheelBatteryHandler.cs
@@ -11,13 +11,17 @@ namespace BluetoothOnewheelAccess.Classes.ValueHandler
         #region --Attributes--
         private DateTime lastToastPoped5Percent;
         private DateTime lastToastPoped20Percent;
+        private DateTime lastToastPoped100Percent;
         private readonly ToastAudio ALARM_SOUND_5_PERCENT;
         private readonly ToastAudio ALARM_SOUND_20_PERCENT;
+        private readonly ToastAudio ALARM_SOUND_100_PERCENT;
 
         private const uint BATTERY_THRESHOLD_5_PERCENT = 5;
         private const uint BATTERY_THRESHOLD_20_PERCENT = 20;
+        private const uint BATTERY_THRESHOLD_100_PERCENT = 100;
         private const uint TOAST_POP_TIMEOUT_SECONDS_5_PERCENT = 30;
         private const uint TOAST_POP_TIMEOUT_SECONDS_20_PERCENT = 30;
+        private const uint TOAST_POP_TIMEOUT_SECONDS_100_PERCENT = 30;
 
         private uint lastValue;
 
@@ -42,6 +46,11 @@ namespace BluetoothOnewheelAccess.Classes.ValueHandler
                 Loop = false,
                 Src = new Uri("ms-winsoundevent:Notification.IM")
             };
+            this.ALARM_SOUND_100_PERCENT = new ToastAudio
+            {
+                Loop = false,
+                Src = new Uri("ms-winsoundevent:Notification.Default")
+            };
         }
 
         #endregion
@@ -56,6 +65,7 @@ namespace BluetoothOnewheelAccess.Classes.ValueHandler
         {
             lastToastPoped5Percent = DateTime.MinValue;
             lastToastPoped20Percent = DateTime.MinValue;
+            lastToastPoped100Percent = DateTime.MinValue;
             lastValue = uint.MaxValue;
         }
 
@@ -66,7 +76,7 @@ namespace BluetoothOnewheelAccess.Classes.ValueHandler
                 return;
             }
 
-            if (value >= BATTERY_THRESHOLD_5_PERCENT && lastValue > BATTERY_THRESHOLD_5_PERCENT)
+            if (value <= BATTERY_THRESHOLD_5_PERCENT && lastValue > BATTERY_THRESHOLD_5_PERCENT)
             {
                 if(timestamp.Subtract(lastToastPoped5Percent).TotalSeconds  > TOAST_POP_TIMEOUT_SECONDS_5_PERCENT)
                 {
@@ -74,12 +84,20 @@ namespace BluetoothOnewheelAccess.Classes.ValueHandler
                     popToast(generateToastContent(value));
                 }
             }
-            else if (value >= BATTERY_THRESHOLD_20_PERCENT && lastValue > BATTERY_THRESHOLD_20_PERCENT)
+            else if (value <= BATTERY_THRESHOLD_20_PERCENT && lastValue > BATTERY_THRESHOLD_20_PERCENT)
             {
                 if (timestamp.Subtract(lastToastPoped20Percent).TotalSeconds > TOAST_POP_TIMEOUT_SECONDS_20_PERCENT)
                 {
                     lastToastPoped20Percent = timestamp;
                     popToast(generateToastContent(value));
+                }
+            }
+            else if( value >= BATTERY_THRESHOLD_100_PERCENT && lastValue < BATTERY_THRESHOLD_100_PERCENT)
+            {
+                if (timestamp.Subtract(lastToastPoped100Percent).TotalSeconds > TOAST_POP_TIMEOUT_SECONDS_100_PERCENT)
+                {
+                    lastToastPoped100Percent = timestamp;
+                    popToast(generateToastFullBatteryContent(value));
                 }
             }
 
@@ -114,7 +132,35 @@ namespace BluetoothOnewheelAccess.Classes.ValueHandler
 
                     new AdaptiveText()
                     {
-                        Text = value + "% left",
+                        Text = value + "% charge left",
+                    }
+                }
+                    }
+                }
+            };
+        }
+
+        private ToastContent generateToastFullBatteryContent(uint value)
+        {
+            return new ToastContent()
+            {
+                Scenario = ToastScenario.Default,
+                Audio = ALARM_SOUND_100_PERCENT,
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                {
+                    new AdaptiveText()
+                    {
+                        Text = "Battery charged 🔋!"
+                    },
+
+                    new AdaptiveText()
+                    {
+                        Text = value + "% charge left",
                     }
                 }
                     }
